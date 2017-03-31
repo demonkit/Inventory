47754551bb57c6bbfc1875048d07372fa6e30c16
demonkit <yydemonkit@gmail.com>
2017-03-31 15:27:11 +0800

    chang

--- multi_small_files.py
+++ multi_small_files.py
@@ -14,36 +14,37 @@ import re
 
 class MultiFileWriter(object):
 
-    def __init__(self, file_path, max_size=524288000):
+    def __init__(self, max_size=524288000):
         """
         Note: the unit of the argument max_size is byte.
         """
-        self.file_path = file_path
         self.max_size = max_size
 
     def _get_current(self, base_dir, filename):
-        pattern = re.compile(r"%s-(\d*)" % filename)
-        existing_idx = []
-        for filename in os.listdir(base_dir):
-            matched = pattern.match(filename)
+        pattern = re.compile(r"^%s-(\d*)" % filename)
+        existing_idx_list = []
+        for name in os.listdir(base_dir):
+            matched = pattern.match(name)
             if matched:
                 idx = matched.groups()[0]
-                existing_idx.append(idx)
-        current_idx = 0
-        if existing_idx:
-            current_idx = max([int(i) for i in existing_idx])
-        current_file = os.path.join(base_dir, filename)
+                existing_idx_list.append(int(idx))
+        if existing_idx_list:
+            current_idx = max(existing_idx_list)
+            current_file = os.path.join(base_dir,
+                                        "%s-%s" % (filename, current_idx))
+        else:
+            current_idx = 0
+            current_file = os.path.join(base_dir, filename)
         return current_idx, current_file
 
-    def get_file_path(self):
-        if not os.path.exists(self.file_path):
-            return self.file_path
+    def get_file_path(self, file_path):
+        if not os.path.exists(file_path):
+            return file_path
 
-        base_dir = os.path.dirname(self.file_path)
-        filename = os.path.basename(self.file_path)
+        base_dir = os.path.dirname(file_path)
+        filename = os.path.basename(file_path)
 
         current_idx, current_file = self._get_current(base_dir, filename)
         if os.stat(current_file).st_size < self.max_size:
             return current_file
-        return "%s-%s" % (self.file_path, current_idx + 1)
-
+        return "%s-%s" % (file_path, current_idx + 1)
